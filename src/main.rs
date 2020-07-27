2f9f5a8ba08819c336231dce9cfc57b5a1f6e9e3
xStrato <metalofgear@hotmail.com>
2020-07-27 13:59:24 -0300

    feat: completed the guessing game

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,42 @@
 use std::io::stdin;
+use std::cmp::Ordering;
+use rand::{thread_rng, Rng};
 
 fn main() {
-    println!("Guess the number");
-    println!("Please input your guess");
+    println!("Guess the number between 0 and 10!");
 
-    let mut guess = String::new();
+    let number = thread_rng().gen_range(0, 11);
 
-    stdin()
-    .read_line(&mut guess)
-    .expect("Failed to read line");
+    loop 
+    {
+        println!("Please input your guess: ");
 
-    println!("You guessed {}", guess);
+        let mut guess = String::new();
+
+        match stdin().read_line(&mut guess)
+        {
+            Ok(_) => println!(""),
+            Err(error) => println!("An error occured: {}", error)
+        };
+
+        let guess: u32 = match guess.trim().parse()
+        {
+            Ok(num) => num,
+            Err(error) => {
+                println!("{} is not a number!", error);
+                continue;
+            }
+        };
+
+        match guess.cmp(&number) 
+        {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You guessed right {} and Win!", number);
+                break;
+            },
+        };
+        println!();
+    }
 }
